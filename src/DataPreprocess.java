0f4260685f45a7b33f97f31d277620bcfb6f064c
candy <1391936847@qq.com>
2015-11-16 10:49:40 +0800

    readfiledone

--- src/DataPreprocess.java
+++ src/DataPreprocess.java
@@ -1,9 +1,8 @@
-
-
-
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
@@ -12,6 +11,7 @@ import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.util.Version;
 /**
  * Created by candy on 2015/11/15.
+ * 读取数据并且分词
  */
 public class DataPreprocess {
     private String filename;
@@ -21,68 +21,63 @@ public class DataPreprocess {
         dir = new File(filename);
     }
 
-    private void Analyzer(String testString){
+    /**
+     * 直接对文件进行分词
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    private static List Analyzer(File file)throws IOException{
         List<String> list = new ArrayList<>();
+        BufferedReader reader;
         try{
-            StringReader reader = new StringReader(testString);
-            FileReader stopWords = new FileReader("./resource/stopword.txt");
-            Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_20,stopWords);
+            reader = new BufferedReader(new FileReader(file));
+            Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_36);
             TokenStream ts = analyzer.tokenStream("", reader);
             ts.addAttribute(CharTermAttribute.class);
             while (ts.incrementToken()) {
                 CharTermAttribute charTermAttribute = ts.getAttribute(CharTermAttribute.class);
                 String word = charTermAttribute.toString();
                 list.add(word);
-                System.out.println(charTermAttribute.toString());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return list;
     }
 
-    public static StringBuilder readFileByLines(File file) {
-        StringBuilder s = new StringBuilder();
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader(file));
-            String tempString;
-            while ((tempString = reader.readLine()) != null) {
-                s.append(tempString);
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e1) {
-                }
-            }
-        }
-        return s;
-    }
-
-    public List<String> readFile(){
-        List<String> list = new ArrayList<>();
-
+    /**
+     * 读文件
+     * @return
+     * @throws IOException
+     */
+    public Map readFile() throws IOException {
+        Map<List,String> map = new HashMap<>();
         File[] files = dir.listFiles();
         for(File file:files){
             String classfiction = file.getName();
             System.out.println(classfiction);
             File[] texts = file.listFiles();
             for(File text: texts){
-
-                StringBuilder s = readFileByLines(text);
-                if(s!=null){
-                    list.add(s.toString());
+                List listWords = Analyzer(text);
+                if(!listWords.isEmpty()){
+                    map.put(listWords,classfiction);
                 }
             }
         }
-        return list;
+        return map;
     }
 
-    public static void main(String[] args){
+    /**
+     *特征词提取
+     * @param records
+     * @return
+     */
+    public List getSpecificWord(Map records){
+        return null;
+    }
+
+    public static void main(String[] args) throws IOException {
         DataPreprocess dataPreprocess = new DataPreprocess("mini_newsgroups");
         dataPreprocess.readFile();
     }
--- src/TFIDF.java
+++ src/TFIDF.java
@@ -1,5 +1,7 @@
 /**
  * Created by candy on 2015/11/15.
+ * ÌáÈ¡ÌØÕ÷´Ê
  */
 public class TFIDF {
+
 }
